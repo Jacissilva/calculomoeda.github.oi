5e213c1448e9e5387f223b982d027750dd2bd112
Jaci Souza <jacissilva1964@gmail.com>
2021-11-16 00:42:07 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+//import 'node_modules/bootstrap/scss/bootstrap.scss';
+//import 'node_modules/bootstrap-vue/src/index.scss';
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
